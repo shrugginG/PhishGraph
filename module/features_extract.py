fbac1109c91273f3ebcd0cc5846cc09faa6a7d95
shrugginG <1245280461@qq.com>
2024-09-24 20:55:09 +0800

    Finish the 17 features extract

--- module/features_extract.py
+++ module/features_extract.py
@@ -37,6 +37,90 @@ def f4_calculate_pct_secondary_red_vertices(G, query_url):
     return pct_secondary_red
 
 
+def f5_calculate_num_secondary_vertices_with_html_present(G, query_url):
+    secondary_vertices_with_html_present = [
+        node
+        for node in G.nodes
+        if node != query_url and G.nodes[node].get("content_type") == "text/html"
+    ]
+    return len(secondary_vertices_with_html_present)
+
+
+def f6_calculate_in_degree_query_vertex(G, query_url):
+    in_degree = G.in_degree(query_url)
+    return in_degree
+
+
+def f7_calculate_out_degree_query_vertex(G, query_url):
+    out_degree = G.out_degree(query_url)
+    return out_degree
+
+
+def f8_calculate_in_degree_query_vertex_no_self_loop(G, query_url):
+    total_in_degree = G.in_degree(query_url)
+    if G.has_edge(query_url, query_url):
+        total_in_degree -= 1
+    return total_in_degree
+
+
+def f9_calculate_in_degree_centrality_mean(G):
+    in_degree_centrality = nx.in_degree_centrality(G)
+    return sum(in_degree_centrality.values()) / len(in_degree_centrality)
+
+
+def f10_calculate_out_degree_centraliy_mean(G):
+    out_degree_centrality = nx.out_degree_centrality(G)
+    print(out_degree_centrality)
+    return sum(out_degree_centrality.values()) / len(out_degree_centrality)
+
+
+def f11_calculate_density_graph(G):
+    return nx.density(G)
+
+
+def f12_calculate_density_red_vertex_subgraph(G):
+    red_vertices = [node for node in G.nodes if G.nodes[node].get("color") == "red"]
+    red_subgraph = G.subgraph(red_vertices)
+    density_red_subgraph = nx.density(red_subgraph)
+
+    return density_red_subgraph
+
+
+def f13_calculate_edge_betweenness_centrality(G):
+    edge_betweenness_centrality = nx.edge_betweenness_centrality(G)
+    return (
+        sum(edge_betweenness_centrality.values()) / len(edge_betweenness_centrality)
+        if len(edge_betweenness_centrality) > 0
+        else 0
+    )
+
+
+def f14_calculate_path_back_to_query_webpage(G, query_url):
+    for node in G.nodes:
+        if node != query_url and nx.has_path(G, node, query_url):
+            return 1
+    return 0
+
+
+def f15_calculate_is_semiconnected(G):
+    if nx.is_semiconnected(G):
+        return 1
+    else:
+        return 0
+
+
+def f16_calculate_num_strongly_connected_components(G):
+    # stroncom = list(nx.strongly_connected_components(G))
+    # print(stroncom)
+    return nx.number_strongly_connected_components(G)
+
+
+def f17_calculate_num_attracting_components(G):
+    # attcom = list(nx.attracting_components(G))
+    # print(attcom)
+    return nx.number_attracting_components(G)
+
+
 if __name__ == "__main__":
     query_url = "https://www.example.com/"
     G = nx.read_graphml(
@@ -47,4 +131,18 @@ if __name__ == "__main__":
     f2 = f2_calculate_pct_blue_vertices(G)
     f3 = f3_calculate_pct_green_vertices(G)
     f4 = f4_calculate_pct_secondary_red_vertices(G, query_url)
-    print(f4)
+    f5 = f5_calculate_num_secondary_vertices_with_html_present(G, query_url)
+    f6 = f6_calculate_in_degree_query_vertex(G, query_url)
+    f7 = f7_calculate_out_degree_query_vertex(G, query_url)
+    f8 = f8_calculate_in_degree_query_vertex_no_self_loop(G, query_url)
+    f9 = f9_calculate_in_degree_centrality_mean(G)
+    f10 = f10_calculate_out_degree_centraliy_mean(G)
+    f11 = f11_calculate_density_graph(G)
+    f12 = f12_calculate_density_red_vertex_subgraph(G)
+    f13 = f13_calculate_edge_betweenness_centrality(G)
+    f14 = f14_calculate_path_back_to_query_webpage(G, query_url)
+    f15 = f15_calculate_is_semiconnected(G)
+    f16 = f16_calculate_num_strongly_connected_components(G)
+    f17 = f17_calculate_num_attracting_components(G)
+
+    print(f17)
--- module/sample_ego_network_build.py
+++ module/sample_ego_network_build.py
@@ -5,14 +5,14 @@ G = nx.DiGraph()
 
 query_url = "https://www.example.com/"
 
-G.add_node(query_url, color="red")  # query_url 赋予红色
-G.add_node("https://www.example.com/login", color="red")  # 同域名的内部链接，红色
-G.add_node("https://www.example.com/register", color="red")
-G.add_node("https://www.external.com/news", color="blue")  # 外部链接，蓝色
+G.add_node(query_url, color="red")
+G.add_node("https://www.example.com/login", color="red", content_type="text/html")
+G.add_node("https://www.example.com/register", color="red", content_type="text/html")
+G.add_node("https://www.external.com/news", color="blue")
 G.add_node("https://www.another.com/about", color="blue")
-G.add_node("https://www.example.com/help", color="red")
+G.add_node("https://www.example.com/help", color="red", content_type="text/html")
 # G.add_node("https://sub.example.com/contact", color="red")
-G.add_node("https://sub.external.com/", color="green")  # 二级外部链接，绿色
+G.add_node("https://sub.external.com/", color="green")
 G.add_node("https://www.another.com/support", color="green")
 
 G.add_edges_from(
